db0d0e62b337528463664bca8bc3314efc4f55aa
shezhangzhang <ischenkan@outlook.com>
2022-07-28 13:57:00 +0800

    refactor: refactor the whole thing

--- src/decoration-manager.ts
+++ src/decoration-manager.ts
@@ -10,7 +10,6 @@ export default class DecorationManager {
   >;
   private timeout: NodeJS.Timer | undefined = undefined;
   private fileName: string = "";
-  private fileLineCount: number = 0;
 
   constructor($activeEditor: vscode.TextEditor | undefined, $fullToken: any) {
     this.activeEditor = $activeEditor;
@@ -21,16 +20,19 @@ export default class DecorationManager {
   setActiveEditor(editor: vscode.TextEditor) {
     this.activeEditor = editor;
     this.fileName = editor.document.fileName;
-    this.fileLineCount = editor.document.lineCount;
   }
 
   triggerUpdateDecorations(
     throttle: boolean = false,
     isEdit: boolean = false,
+    diffLine: number = 0,
     startLine?: number,
     endLine?: number
   ) {
     console.log("fileName", this.fileName);
+    /**
+     * Active editor changed event
+     */
     if (!isEdit) {
       if (this.fileDecorationMap.has(this.fileName)) {
         this.clearCurrentFileDecoration();
@@ -45,41 +47,43 @@ export default class DecorationManager {
 
     if (throttle) {
       this.timeout = setTimeout(() => {
-        this.setupDecorations(startLine, endLine);
+        this.setupDecorations(diffLine, startLine, endLine);
       }, 500);
     } else {
-      this.setupDecorations(startLine, endLine);
+      this.setupDecorations(diffLine, startLine, endLine);
     }
   }
 
-  setupDecorations(startLine?: number, endLine?: number) {
+  setupDecorations(diffLine: number, startLine?: number, endLine?: number) {
     if (startLine && endLine) {
-      console.log("editing", startLine, endLine);
-      const currentLineCount = this.activeEditor!.document.lineCount;
-      const diffLine = currentLineCount - this.fileLineCount;
       console.log("diffLine", diffLine);
 
+      const lines = this.getLines(startLine, endLine);
       if (diffLine < 0) {
-        const lines = this.getLines(startLine, endLine);
-        return this.clearCurrentFileDecoration(lines);
+        this.clearCurrentFileDecoration(lines);
+        this.updateFileDecorationMap(diffLine, startLine);
       }
 
       if (diffLine === 0) {
         this.clearCurrentFileDecoration([startLine]);
+        this.setDecorations([startLine]);
       }
-    }
 
-    this.setDecorations();
+      if (diffLine > 0) {
+        this.updateFileDecorationMap(diffLine, startLine);
+        this.setDecorations(lines);
+      }
+    } else {
+      this.setDecorations();
+    }
   }
 
-  setDecorations() {
+  setDecorations(sepecificLines?: number[]) {
     console.log("!!!!!!updating!!!!!");
     const text = this.activeEditor!.document.getText();
     const fullTokenKeys = Object.keys(this.fullToken);
-    const lineDecorationMap = new Map<
-      number,
-      vscode.TextEditorDecorationType[]
-    >();
+    const lineDecorationMap: Map<number, vscode.TextEditorDecorationType[]> =
+      this.fileDecorationMap.get(this.fileName) || new Map();
 
     if (!this.fileDecorationMap.has(this.fileName)) {
       this.fileDecorationMap.set(this.fileName, new Map());
@@ -94,7 +98,12 @@ export default class DecorationManager {
           match.index
         ).line;
 
-        this.setDecoration(match, key, lineDecorationMap);
+        if (
+          !sepecificLines ||
+          (sepecificLines && sepecificLines.includes(currentLine))
+        ) {
+          this.setDecoration(match, key, lineDecorationMap);
+        }
       }
     });
 
@@ -104,12 +113,15 @@ export default class DecorationManager {
 
   clearCurrentFileDecoration(lines?: number[]) {
     const lineDecorationMapItem = this.fileDecorationMap.get(this.fileName);
-
-    if (lineDecorationMapItem) {
+    console.log("lineDecorationMapItem", lineDecorationMapItem);
+    if (lineDecorationMapItem?.size) {
       if (lines) {
         lines.forEach((line) => {
           console.log("dispose line", line);
-          lineDecorationMapItem.get(line)?.forEach(this.dispose);
+          if (lineDecorationMapItem.has(line)) {
+            lineDecorationMapItem.get(line)?.forEach(this.dispose);
+            lineDecorationMapItem.delete(line);
+          }
         });
       } else {
         lineDecorationMapItem.forEach((value) => {
@@ -181,5 +193,28 @@ export default class DecorationManager {
     );
     // console.log("setDecorations", currentLine);
     this.activeEditor!.setDecorations(decorationType, valueDecorations);
+  }
+
+  updateFileDecorationMap(diffLine: number, startLine: number) {
+    const lineDecorationMapItem = this.fileDecorationMap.get(this.fileName);
+    const newMap: Map<number, vscode.TextEditorDecorationType[]> = new Map();
+    console.log("before: ", lineDecorationMapItem);
+    console.log("startLine", startLine);
+
+    if (lineDecorationMapItem?.size) {
+      lineDecorationMapItem.forEach((value, key) => {
+        if (key >= startLine) {
+          newMap.set(
+            diffLine > 0 ? key + diffLine : key - Math.abs(diffLine),
+            lineDecorationMapItem.get(key)!
+          );
+        } else {
+          newMap.set(key, lineDecorationMapItem.get(key)!);
+        }
+      });
+    }
+
+    console.log("after: ", newMap);
+    this.fileDecorationMap.set(this.fileName, newMap);
   }
 }
--- src/listener.ts
+++ src/listener.ts
@@ -5,10 +5,13 @@ export default function setupEventListener(
   context: vscode.ExtensionContext,
   fullToken: any
 ) {
+  let diffLine: number = 0;
+  let fileLineCount: number = 0;
   let activeEditor = vscode.window.activeTextEditor;
   const decorationManager = new DecorationManager(activeEditor, fullToken);
 
   if (activeEditor) {
+    fileLineCount = activeEditor.document.lineCount;
     decorationManager.setActiveEditor(activeEditor);
     decorationManager.triggerUpdateDecorations();
   }
@@ -19,14 +22,23 @@ export default function setupEventListener(
         return;
       }
 
-      const startLine = event.contentChanges[0].range.start.line;
-      const endLine = event.contentChanges[0].range.end.line;
-
       if (activeEditor && event.document === activeEditor.document) {
+        console.log("change", event.contentChanges[0]);
+
+        diffLine = activeEditor.document.lineCount - fileLineCount;
+        fileLineCount = activeEditor.document.lineCount;
+
+        const [startLine, endLine] = getStartEndLine(
+          event.document,
+          event.contentChanges[0]
+        );
+
+        console.log("editing", startLine, endLine);
         decorationManager.setActiveEditor(activeEditor);
         decorationManager.triggerUpdateDecorations(
+          diffLine === 0,
           true,
-          true,
+          diffLine,
           startLine,
           endLine
         );
@@ -40,6 +52,7 @@ export default function setupEventListener(
     (editor) => {
       activeEditor = editor;
       if (editor) {
+        fileLineCount = editor.document.lineCount || 0;
         decorationManager.setActiveEditor(editor);
         decorationManager.triggerUpdateDecorations();
       }
@@ -47,4 +60,60 @@ export default function setupEventListener(
     null,
     context.subscriptions
   );
+
+  function getStartEndLine(
+    document: vscode.TextDocument,
+    change: vscode.TextDocumentContentChangeEvent
+  ) {
+    const newLines = change.text.split("\n").length;
+    let startLine = change.range.start.line;
+    let endLine = change.range.end.line;
+    const startLinePos = document.lineAt(startLine);
+
+    const isStartLineEmpty = startLinePos.isEmptyOrWhitespace;
+    const startLineLastPos = new vscode.Position(
+      startLine,
+      Math.max(startLinePos.text.length, 0)
+    );
+    const isStartLineLastPos = startLineLastPos.isEqual(change.range.start);
+
+    console.log("original", startLine, endLine);
+    console.log("isStartLineEmpty", isStartLineEmpty);
+    console.log("isStartLineLastPos", isStartLineLastPos);
+
+    /**
+     * paste multiple lines
+     * there are some `\n` in `text`, but currentChange[0].range is always a single line
+     */
+    if (newLines > 1 && !change.text.startsWith("\n")) {
+      endLine = startLine + newLines - 1;
+    }
+
+    /**
+     * cmd + x
+     */
+    if (
+      change.range.start.character === 0 &&
+      change.range.end.character === 0
+    ) {
+      endLine -= 1;
+    }
+
+    /**
+     * enter
+     */
+    if (!isStartLineEmpty && diffLine === 1) {
+      startLine += 1;
+      endLine += 1;
+    }
+
+    /**
+     * delete to the end of the previous line
+     */
+    if (!isStartLineEmpty && isStartLineLastPos && diffLine < 0) {
+      startLine += 1;
+    }
+
+    return [startLine, endLine];
+  }
 }
